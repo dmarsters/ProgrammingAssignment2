b15fc9d552dedff827f008b70ce2899332e70e1c
dmarsters <dal.marsters@gmail.com>
2018-10-13 17:07:28 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author: Dal Marsters
+## makeCacheMatrix returns a list of methods enabling you to
+## makeMatrix - pass in a matrix and store it in an environment variable
+## getMatrix - retrieves the stored matrix
+## cacheSolve - calculates and returns the inverse of the stored matrix 
+## or calculates the inverse a new matrix and stores it
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(outside_matrix = matrix()) {
+  makeMatrix <- function(given_matrix) {
+    if (identical(given_matrix, outside_matrix)){
+      #if the passed in matrix == the saved matrix, do nothing
+       print("true")
+    } else {
+      #make the passed matrix the saved matrix
+        outside_matrix <<- given_matrix
+        #empty out the stored inverse
+        inverse_cache <<- NULL
+    }
+  }
+  
+  #getMatrix retrieves outside scope x 
+  getMatrix <- function() outside_matrix
+  
+  #if inverse exists, return it
+  #else calculate new inverse, store it, and return it
+  cacheSolve <- function(){
+    if (!is.null(inverse_cache)){                   # if an inverse exists, 
+        return(inverse_cache) 						# return inverse
+    } else {                                      
+        inverse_cache <<- solve(outside_matrix)   	# calculate and store inverse
+        return(inverse_cache) 						# return stored inverse
+    }
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #returns list of methods
+  list(makeMatrix = makeMatrix, getMatrix = getMatrix, cacheSolve = cacheSolve)
 }
